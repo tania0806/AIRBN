b669f31a73156eb719b71c28fa88d4e83cbb969d
tania0806 <13347@utl.edu.mx>
2025-03-13 13:35:50 -0600

    ...

--- Models/FiltrosModel.cs
+++
@@ -1,9 +0,0 @@
-namespace reportesApi.Models;
-
-
-public class FiltrosFechaModel
-{
-    public string FechaInicial {get;set ;}
-    public string FechaFinal {get;set ;}
-    public int IdSucursal {get; set;}
-}
